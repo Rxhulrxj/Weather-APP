ab46b55886ff968475065cc98df0f916df1c87d6
Rahul Raj N <88202340+Rxhulrxj@users.noreply.github.com>
2021-11-06 22:33:27 +0530

    hardcoded need to connect api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
 import "./App.css";
+const Api = {
+  key: "98ff436b7c20596466311487b2531a16",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  return <div className="App">hiiii</div>;
+  const datebuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div className="App">
+      <div className="Main">
+        <img
+          src="https://source.unsplash.com/1920x1080?nature"
+          alt="Background"
+          className="Background"
+        />
+        <div className="weather-container">
+          <div className="Search">
+            <input
+              type="text"
+              className="Search-bar"
+              placeholder="Search here..."
+            />
+          </div>
+          <div className="weather-info">
+            <div className="location">Trivandrum,Kerala,India</div>
+            <div className="date">{datebuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">15&deg;</div>
+            <div className="weather">Sunny</div>
+          </div>
+        </div>
+      </div>
+      <div className="mobscreen">
+        <img src={require("./Assests/Oops.gif").default} alt="oops" />
+        <h4>
+          Sorry <br /> This site is Currently unavailable for Mobile Devices
+        </h4>
+      </div>
+    </div>
+  );
 }
 
 export default App;
