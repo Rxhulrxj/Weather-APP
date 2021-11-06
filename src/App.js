50b291fbc41e4d7e9806c0698c94064ec51e4ac5
Rahul Raj N <88202340+Rxhulrxj@users.noreply.github.com>
2021-11-07 01:46:18 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 const Api = {
   key: "98ff436b7c20596466311487b2531a16",
@@ -5,6 +6,20 @@ const Api = {
 };
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${Api.base}weather?q=${query}&units=metric&APPID=${Api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
   const datebuilder = (d) => {
     let months = [
       "January",
@@ -49,16 +64,44 @@ function App() {
               type="text"
               className="Search-bar"
               placeholder="Search here..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
             />
           </div>
-          <div className="weather-info">
-            <div className="location">Trivandrum,Kerala,India</div>
-            <div className="date">{datebuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">15&deg;</div>
-            <div className="weather">Sunny</div>
-          </div>
+          {typeof weather.main != "undefined" ? (
+            <div>
+              <div className="weather-info">
+                <div className="location">
+                  {weather.name},{weather.sys.country}
+                </div>
+                <div className="date">{datebuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">
+                  {Math.round(weather.main.temp)}&nbsp;&deg;C
+                </div>
+                <div className="additional-info">
+                  <label className="feel">Feels like:</label>&nbsp;
+                  {Math.round(weather.main.feels_like)}&nbsp;&deg;C&nbsp;&nbsp;
+                  <label className="humidity">Humidity:</label>&nbsp;
+                  {Math.round(weather.main.humidity)}&nbsp;%
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+                <div className="add-info">
+                  <label className="wind">Wind:</label>&nbsp;
+                  {Math.round(weather.wind.speed)}&nbsp;m/s&nbsp;&nbsp;
+                  <label className="visibility">visibility:</label>&nbsp;
+                  {Math.round(weather.visibility / 1000)}&nbsp;km
+                  <br />
+                  <label className="pressure">Pressure:</label>&nbsp;
+                  {Math.round(weather.main.pressure)}&nbsp;mb
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
       <div className="mobscreen">
